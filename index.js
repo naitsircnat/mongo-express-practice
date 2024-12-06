c4dcc5525e4bb4c65620608be00cb1c55451c6a6
naitsircnat <cristiangerardchen@gmail.com>
2024-12-06 08:49:48 +0000

    Added search and post routes

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ async function connect(mongoURI, dbName) {
 async function main() {
   let db = await connect(mongoURI, "sample_supplies");
 
+  // Get all sales
   app.get("/sales", async (req, res) => {
     try {
       const sales = await db
@@ -30,7 +31,7 @@ async function main() {
           items: 1,
           "customer.email": 1,
         })
-        .limit(20)
+        .limit(10)
         .toArray();
       res.json(sales);
     } catch (error) {
@@ -38,6 +39,8 @@ async function main() {
       res.status(500).json({ error: "Internal server error" });
     }
   });
+
+  // Get a specific sale
 
   const { ObjectId } = require("mongodb");
 
@@ -62,6 +65,88 @@ async function main() {
       res.json(sale);
     } catch (error) {
       console.error("Error: ", error);
+      res.status(500).json({ Error: "Internal server error" });
+    }
+  });
+
+  // Search engine
+  app.get("/search", async (req, res) => {
+    try {
+      const { purchaseMethod, item, storeLocation } = req.query;
+      // const { purchaseMethod, storeLocation } = req.query;
+
+      let query = {};
+
+      if (purchaseMethod) {
+        query["purchaseMethod"] = { $regex: purchaseMethod, $options: "i" };
+      }
+
+      if (item) {
+        query["items.name"] = { $regex: item, $options: "i" };
+      }
+
+      if (storeLocation) {
+        query["storeLocation"] = { $regex: storeLocation, $options: "i" };
+      }
+
+      let sales = await db
+        .collection("sales")
+        .find(query)
+        .project({ items: 1, storeLocation: 1, purchaseMethod: 1 })
+        .limit(10)
+        .toArray();
+
+      res.json(sales);
+    } catch (error) {
+      console.error("Error searching sales", error);
+      res.status(500).json({ Error: "Internal Server Error" });
+    }
+  });
+
+  // Post route
+  /*
+  - Get ObjectID object from mongodb
+  - Create app.post with try and catch 
+  - Create variables to store respective data to be inserted
+  - Perform validation to check that all required data are provided
+  - Insert data into collection
+  - Create response that contains message and id of inserted object
+  */
+
+  app.post("/new", async (req, res) => {
+    try {
+      const { items, storeLocation, customer, couponUsed, purchaseMethod } =
+        req.body;
+
+      if (
+        !items ||
+        !storeLocation ||
+        !customer ||
+        !couponUsed ||
+        !purchaseMethod
+      ) {
+        return res
+          .status(400)
+          .json({ Error: "Please provide all required data" });
+      }
+
+      const newSale = {
+        saleDate: new Date(),
+        items,
+        storeLocation,
+        customer,
+        couponUsed,
+        purchaseMethod,
+      };
+
+      const result = await db.collection("sales").insertOne(newSale);
+
+      res.json({
+        message: "Sale inserted",
+        id: result.insertedId,
+      });
+    } catch (error) {
+      console.error("Error adding sale", error);
       res.status(500).json({ Error: "Internal server error" });
     }
   });
---
+++ newSale.js
@@ -0,0 +1,26 @@
+const newS = {
+  date: "2024-12-06T02:13:51.893+00:00",
+  items: [
+    {
+      name: "eraser",
+      tags: ["school", "work"],
+      price: 1.0,
+      quantity: 2,
+    },
+    {
+      name: "pen",
+      tags: ["school", "work", "home"],
+      price: 2.0,
+      quantity: 3,
+    },
+  ],
+  storeLocation: "Singapore",
+  customer: {
+    gender: "M",
+    age: 40,
+    email: "roger@roger.com",
+    satisfaction: 5,
+  },
+  couponUsed: true,
+  purchaseMethod: "Online",
+};
